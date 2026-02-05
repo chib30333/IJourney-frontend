6b2b71dcde0a25c15033e3badd9c19565b32a596
chib30333 <chib30333@gmail.com>
2026-02-05 04:42:09 +0100

    feat:add Loading spinner component

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,30 +1,24 @@
-import React from 'react';
-import { motion } from 'framer-motion';
-
-const LoadingSpinner: React.FC = () => {
-    const spinnerVariants = {
-        animate: {
-            rotate: 360,
-        },
-        transition: {
-            duration: 1,
-            ease: 'linear',
-            repeat: Infinity,
-        },
-    };
-
+function LoadingSpinner() {
     return (
-        <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
-            <motion.div
-                className="w-16 h-16 border-4 border-t-4 border-blue-500 rounded-full"
-                variants={spinnerVariants}
-                animate="animate"
-                transition={{
-                    duration: 1,
-                    ease: 'linear',
-                    repeat: Infinity,
-                }}
-            ></motion.div>
+        <div className="overlay-panel">
+            <svg
+                id="svg-spinner"
+                xmlns="http://www.w3.org/2000/svg"
+                width="96"
+                height="96"
+                viewBox="0 0 48 48"
+            >
+                <circle cx="24" cy="4" r="4" fill="#000" />
+                <circle cx="12.19" cy="7.86" r="3.7" fill="#171717" />
+                <circle cx="5.02" cy="17.68" r="3.4" fill="#2f2f2f" />
+                <circle cx="5.02" cy="30.32" r="3.1" fill="#464646" />
+                <circle cx="12.19" cy="40.14" r="2.8" fill="#5e5e5e" />
+                <circle cx="24" cy="44" r="2.5" fill="#757575" />
+                <circle cx="35.81" cy="40.14" r="2.2" fill="#8d8d8d" />
+                <circle cx="42.98" cy="30.32" r="1.9" fill="#a4a4a4" />
+                <circle cx="42.98" cy="17.68" r="1.6" fill="#bcbcbc" />
+                <circle cx="35.81" cy="7.86" r="1.3" fill="#d3d3d3" />
+            </svg>
         </div>
     );
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,10 +4,11 @@ import { useAuth } from '../context/AuthContext';
 import Header from '../components/Layout/Header';
 import Footer from '../components/Layout/Footer';
 import Chatbot from '../components/Chatbot';
+import LoadingSpinner from '../components/Loader';
 
 function Layout() {
     const { loading } = useAuth();
-    if (loading) return <div className="p-6 bg-white min-w-screen min-h-screen">Loading…</div>;
+    if (loading) return <LoadingSpinner />;
     return (
         <div className="flex flex-col min-h-screen bg-gray-100 ">
             <Header />
